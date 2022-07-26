3de4386f4ab8ac0dca07a2c440de3b78aa9f0b67
Jennie Evans <jennie1evans@hotmail.com>
2022-07-26 17:00:09 +0100

    mocking and integration tests fixed

--- integration.test.js
+++ integration.test.js
@@ -15,14 +15,13 @@ describe('transaction and statement integration', () => {
     account.makeDeposit(2000.00, "13/01/2023")
     const statement = new Statement(account);
     expect(statement.getTransactions()).toEqual([{date: "10/01/2023", credit: 1000.00, debit: 0, balance: 1000.00}, {date: "13/01/2023", credit: 2000.00, debit: 0, balance: 3000.00}])
-    console.log(account)
   })
 
   it ('prints out statement with one deposit', () => { 
     const account = new BankAccount();
     account.makeDeposit(1000.00, "10/01/2023")
     const statement = new Statement(account);
-    expect(statement.getStatement()).toEqual(["10/01/2023 || 1000.00 ||  || 1000.00"])
+    expect(statement.getStatement()).toEqual(["10/01/2023 || 1000.00 || 0.00 || 1000.00"])
   })
 
   it ('prints out statement for two deposits', () => { 
@@ -30,7 +29,7 @@ describe('transaction and statement integration', () => {
     account.makeDeposit(1000.00, "10/01/2023")
     account.makeDeposit(2000.00, "13/01/2023")
     const statement = new Statement(account);
-    expect(statement.getStatement()).toEqual(["10/01/2023 || 1000.00 ||  || 1000.00", "13/01/2023 || 2000.00 ||  || 3000.00"])
+    expect(statement.getStatement()).toEqual(["10/01/2023 || 1000.00 || 0.00 || 1000.00", "13/01/2023 || 2000.00 || 0.00 || 3000.00"])
   })
 
   it ('prints a statement for deposits and a withdrawal', () => { 
@@ -39,6 +38,7 @@ describe('transaction and statement integration', () => {
     account.makeDeposit(2000.00, "13/01/2023")
     account.makeWithdrawal(500.00, "14/01/2023")
     const statement = new Statement(account);
-    expect(statement.getStatement()).toEqual(["10/01/2023 || 1000.00 ||  || 1000.00", "13/01/2023 || 2000.00 ||  || 3000.00", "14/01/2023 ||  || 500.00 || 2500.00"])
+    expect(statement.getStatement()).toEqual(["10/01/2023 || 1000.00 || 0.00 || 1000.00", "13/01/2023 || 2000.00 || 0.00 || 3000.00", "14/01/2023 || 0.00 || 500.00 || 2500.00"])
+    console.log(statement.getStatement())
   })
 })
--- statement.test.js
+++ statement.test.js
@@ -3,10 +3,24 @@ const BankAccount = require('./bank')
 
 describe('creating a statement', () => {
 
-  it('adds a credit to the statement', () => {
+  it('adds a credit to transactions', () => {
     const mockBankAccount = new BankAccount();
     mockBankAccount.transactions = [{date: "10/01/2023", credit: 1000.00, debit: 0.00, balance: 1000.00}];
     const statement = new Statement(mockBankAccount);
-    expect(statement.getTransactions()).toEqual([{date: "10/01/2023", credit: 1000.00, debit: 0, balance: 1000.00}])
+    expect(statement.getTransactions()).toEqual([{date: "10/01/2023", credit: 1000.00, debit: 0.00, balance: 1000.00}])
+  })
+
+  it ('prints out statement with one deposit', () => { 
+    const mockBankAccount = new BankAccount();
+    mockBankAccount.transactions = [{date: "10/01/2023", credit: 1000.00, debit: 0.00, balance: 1000.00}];
+    const statement = new Statement(mockBankAccount);
+    expect(statement.getStatement()).toEqual(["10/01/2023 || 1000.00 || 0.00 || 1000.00"])
+  })
+
+  it ('prints a statement for deposit and a withdrawal', () => { 
+    const mockBankAccount = new BankAccount();
+    mockBankAccount.transactions = [{date: "10/01/2023", credit: 1000.00, debit: 0.00, balance: 1000.00}, {date: "14/01/2023", credit: 0.00, debit: 500.00, balance: 500.00}];
+    const statement = new Statement(mockBankAccount);
+    expect(statement.getStatement()).toEqual(["10/01/2023 || 1000.00 || 0.00 || 1000.00", "14/01/2023 || 0.00 || 500.00 || 500.00"])
   })
 })
