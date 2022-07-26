0f2f34de82fd27fd02203ee0d3beb374028e1351
Jennie Evans <jennie1evans@hotmail.com>
2022-07-26 12:36:42 +0100

    dates added to integration testing

--- integration.test.js
+++ integration.test.js
@@ -4,16 +4,17 @@ const Statement = require('./statement')
 describe('printing a statement', () => {
   it('adds a credit to the statement', () => {
     const account = new BankAccount();
-    account.makeDeposit(1000)
+    account.makeDeposit(1000, "10/01/2023")
     const statement = new Statement(account);
-    expect(statement.getPrintout()).toEqual([{credit: 1000, balance: 1000}])
+    expect(statement.getPrintout()).toEqual([{date: "10/01/2023", credit: 1000, balance: 1000}])
   })
 
   it('adds a two credit lines to the statement', () => {
     const account = new BankAccount();
-    account.makeDeposit(1000)
-    account.makeDeposit(2000)
+    account.makeDeposit(1000, "10/01/2023")
+    account.makeDeposit(2000, "13/01/2023")
     const statement = new Statement(account);
-    expect(statement.getPrintout()).toEqual([{credit: 1000, balance: 1000}, {credit: 2000, balance: 3000}])
+    expect(statement.getPrintout()).toEqual([{date: "10/01/2023", credit: 1000, balance: 1000}, {date: "13/01/2023", credit: 2000, balance: 3000}])
+    console.log(account)
   })
 })
