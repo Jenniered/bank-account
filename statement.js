6d19fffaef08e3e8e198dfdefad5d9bc64aeac27
Jennie Evans <jennie1evans@hotmail.com>
2022-07-26 22:29:04 +0100

    formatting of statement - unit tests passing

--- statement.js
+++ statement.js
@@ -9,19 +9,30 @@ class Statement {
     return this.account.transactions
   }
 
-  getStatement() {
+  formatStatement() {
     let statement = this.account.transactions.map((object) => { return `${(object.date)} || ${(object.credit.toFixed(2))} || ${(object.debit.toFixed(2))} || ${(object.balance.toFixed(2))}` });
     this.reverseStatement(statement);
     this.addHeader(statement);
-    return statement;
+    const formattedStatement = this.removeZero(statement);
+    return formattedStatement;
   }
-  
+
+  getStatement() {
+    const statementPrintout = this.formatStatement();
+    return statementPrintout
+  }
+
   reverseStatement(statement) {
     statement.reverse();
   }
 
   addHeader(statement) {
     statement.splice(0,0,"date || credit || debit || balance");
+  }
+
+  removeZero(statement) {
+    const final = statement.map((string) => { return string.replaceAll(" 0.00 ", " ") })
+    return final
   }
 }
 
